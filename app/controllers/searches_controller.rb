1406d6ae27eddb54c90fc9cb35f2a533f0d2e615
Josiah Smith <jcsmithf22@gmail.com>
2025-04-21 21:52:32 -0500

    improve form and add table display

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,11 @@
 class SearchesController < ApplicationController
+  before_action :set_search, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @searches = Search.all
+  end
+
+  def show
   end
 
   def new
@@ -8,12 +13,29 @@ class SearchesController < ApplicationController
   end
 
   def create
+    puts search_params
     @search = Search.new(search_params)
     if @search.save
       redirect_to searches_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @search.update(search_params)
+      redirect_to searches_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @search.destroy
+    redirect_to searches_path
   end
 
   private
